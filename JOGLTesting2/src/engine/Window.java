060d8f98e8d217f677b998f359735ac21a9aed36
shalom234 <chickenshalom@gmail.com>
2019-12-09 14:41:35 -0500

    test

--- JOGLTesting2/src/engine/Window.java
+++ JOGLTesting2/src/engine/Window.java
@@ -21,7 +21,7 @@ import com.jogamp.opengl.util.Animator;
 
 @SuppressWarnings("serial")
 public class Window extends JFrame implements GLEventListener {
-	private GLCanvas mainCanvas;
+	private GLCanvas mainCanvas; //test
 	
 	private Animator animator;
 	
